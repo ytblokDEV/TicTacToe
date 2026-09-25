9dca8b8d94455fb9e8b3be012930fbf33840b9ad
ytblok <ytblok484@gmail.com>
2026-09-25 12:29:58 +0200

    chuja dziala ale można postawić "O"

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,40 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[][] board = {
-                {"1", "2", "3"},
-                {"4", "5", "6"},
-                {"7", "8", "9"}
+                { "1", "2", "3" },
+                { "4", "5", "6" },
+                { "7", "8", "9" }
         };
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j]);
+                System.out.print(" ");
             }
             System.out.print("\n");
         }
 
-        for (int k = 0; k < 9; k++) {
-            int rowX;
-            int collumnX;
+        for (int k = 0; k < 5; k++) {
+            // #################################
+            // Wstawianie "X"
+            // #################################
+            int rowX = 0;
+            int collumnX = 0;
             while (true) {
-                System.out.println("podaj pozycję na której chcesz umieścić X");
+                System.out.println("podaj pozycję na której chcesz umieścić X lub exit aby wyjść");
 
-                if (!scanner.hasNextInt()) {
+                String buffer = scanner.nextLine().trim();
+
+                if (buffer.equalsIgnoreCase("exit")) {
+                    System.exit(0);
+                }
+
+                int postionX;
+                try {
+                    postionX = Integer.parseInt(buffer);
+                } catch (NumberFormatException e) {
                     System.out.println("Wybierz liczbę");
-                    scanner.next();
                     continue;
                 }
-                int postionX = scanner.nextInt();
 
                 if (postionX < 1 || postionX > 9) {
                     System.out.println("pozycja poza przedziałem");
@@ -46,13 +57,54 @@ public class Main {
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
                     System.out.print(board[i][j]);
+                    System.out.print(" ");
                 }
                 System.out.print("\n");
             }
+            // #################################
+            // Wstawianie "O"
+            // #################################
+            int rowO;
+            int collumnO;
+            while (true) {
+                System.out.println("podaj pozycję na której chcesz umieścić O lub exit aby wyjść");
 
+                String buffer = scanner.nextLine().trim();
+                if (buffer.equalsIgnoreCase("exit")) {
+                    System.exit(0);
+                }
+
+                int postionO;
+                try {
+                    postionO = Integer.parseInt(buffer);
+                } catch (NumberFormatException e) {
+                    System.out.println("Wybierz liczbę");
+                    continue;
+                }
+
+                if (postionO < 1 || postionO > 9) {
+                    System.out.println("pozycja poza przedziałem");
+                    continue;
+                }
+
+                rowO = (postionO - 1) / 3;
+                collumnO = (postionO - 1) % 3;
+
+                if (board[rowO][collumnO].equals("X") || board[rowO][collumnO].equals("O")) {
+                    System.out.println("to pole jest już zajęte");
+                    continue;
+                }
+                break;
+            }
+            board[rowO][collumnO] = "O";
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(board[i][j]);
+                    System.out.print(" ");
+                }
+                System.out.print("\n");
+            }
         }
-
-
         scanner.close();
     }
 }
